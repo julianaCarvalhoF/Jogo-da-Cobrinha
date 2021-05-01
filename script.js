f81987078f2d9704267f86b43ddd16f8ff4a2d24
julianaCarvalhoF <juliana7001@gmail.com>
2021-05-01 17:17:00 -0300

    definir posicao aleatoria da comidinha

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0]= {
     y: 8* box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)* box,
+    y: Math.floor(Math.random() * 15 + 1)* box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -37,6 +46,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -46,7 +56,15 @@ function iniciarJogo(){
     if(direction == "up")snakeY -=box;
     if(direction == "down")snakeY +=box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1)* box;
+        food.y = Math.floor(Math.random() * 15 + 1)* box ;
+    }
+
+    
 
     let newHead = {
         x: snakeX,
